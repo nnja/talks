d094961bb959aca426c13730808095b1eb21b247
Nina Zakharenko <nzakharenko@gmail.com>
2019-10-10 12:33:38 -0700

    refactor and code cleanup

--- 2019-02-PyCascades/code/adafruit-cpe-initial/code.py
+++ 2019-02-PyCascades/code/adafruit-cpe-initial/code.py
@@ -1,4 +1,8 @@
+# TODO -- Add licensing, copyright, and instructions for using the Visual studio code extension
 # This is forked version of the original demo optimized for the VS Code Circuit Playground Express add-in
+# When the device is running use the command:
+# "Device Simulator Express: Open Serial Monitor" in Visual Studio Code
+# to get read outs from the device
 
 import time
 import random
@@ -9,86 +13,75 @@ TOUCH_PIANO = True
 
 # NeoPixel color names
 WHITE = (50, 50, 50)
-DARKORANGE = (80, 44, 0)
+DARK_ORANGE = (80, 44, 0)
 ORANGE = (244, 117, 33)
-YELLOWORANGE = (216, 59, 1)
-OFF = (0, 0, 0)
+YELLOW_ORANGE = (216, 59, 1)
+BLACK = (0, 0, 0)
 
 # Dim the lights a bit, they're bright
 cpx.pixels.brightness = 0.3
 
-# Play startup noise on boot
+# SPEAKER - Play startup noise on boot
 cpx.play_file("Fanfare.wav")
 
 
-def wheel(pos):
+def wheel(position):
     # Return color value for position
-    if (pos < 0) or (pos > 255):
-        return (0, 0, 0)
-    if pos < 85:
+    if position < 0 or position > 255:
+        return BLACK
+    if position < 85:
         return ORANGE
-    elif pos < 170:
-        pos -= 85
-        return YELLOWORANGE
+    elif position < 170:
+        position -= 85
+        return YELLOW_ORANGE
     else:
-        pos -= 170
+        position -= 170
     return WHITE
 
 
-# Lights on or off
-lights_on = True
-# LED on or off
-led_on = False
-# Counter for all 10 pixels
-pixeln = 0
-# Can tell the switch changed
-last_switch = cpx.switch
-
+lights_on = True  # Lights on or off
+led_on = False  # LED on or off
+current_pixel = 0  # Counter for all 10 pixels
+last_switch = cpx.switch  # Last position of the switch
 
 while True:
-    # LIGHTS
-    # This  makes a swirling pattern of orange colors!
+    # LIGHTS - This  makes a swirling pattern of orange colors!
     if lights_on:
-        for p in range(10):
-            color = wheel(25 * ((pixeln + p) % 10))
-            cpx.pixels[p] = [int(c * ((10 - (pixeln + p) % 10)) / 10.0) for c in color]
+        for pixel_pos in range(10):
+            color = wheel(25 * ((current_pixel + pixel_pos) % 10))
+            cpx.pixels[pixel_pos] = [int(c * ((10 - (current_pixel + pixel_pos) % 10)) / 10.0) for c in color]
 
-        # Each time 'round we tick off one pixel at a time
+        # Each time around we tick off one pixel at a time
         if cpx.switch:  # depending on the switch we'll go clockwise
-            pixeln += 1
-            if pixeln > 9:
-                pixeln = 0
+            current_pixel += 1
+            if current_pixel > 9:
+                current_pixel = 0
         else:  # or counter clockwise, flip the switch to change direction
-            pixeln -= 1
-            if pixeln < 0:
-                pixeln = 9
+            current_pixel -= 1
+            if current_pixel < 0:
+                current_pixel = 9
 
-        # BUTTONS
-        # Press and hold to make the lights dimmer or brighter
-        # Dimmer
+        # BUTTONS - Press and hold to make the lights temporarily dimmer or brighter
         if cpx.button_a:
-            print("Button A pressed")
+            print("Button A pressed - make lights dimmer")
             cpx.pixels.brightness = 0.1
-        # Brighter!
         if cpx.button_b:
-            print("Button B pressed")
+            print("Button B pressed - make lights brighter")
             cpx.pixels.brightness = 0.5
-        # Neither buttons pressed
         if not cpx.button_a and not cpx.button_b:
+            # Go back to default brightness if neither button is pressed
             cpx.pixels.brightness = 0.3
 
-    # SWITCH
-    # Check the switch
+    # SWITCH - Check the switch
     if cpx.switch:
-        if last_switch != cpx.switch:  # if it moved, print it out
+        if last_switch != cpx.switch:
             print("Switch moved left")
     else:
         if last_switch != cpx.switch:
             print("Switch moved right")
     last_switch = cpx.switch
 
-    # CAPACITIVE TOUCH
-    # Touch A1 - A7 on the device to play music
+    # CAPACITIVE TOUCH - Touch A1 - A7 on the device to play music
     if TOUCH_PIANO:
         if cpx.touch_A4:
             cpx.play_tone(524, 0.25)
@@ -105,22 +98,26 @@ while True:
         elif cpx.touch_A3:
             cpx.play_tone(988, 0.25)
 
-    # SENSORS
-    # When the device is running use the command "Device Simulator Express: Open Serial Monitor" to get read outs from the device
-    if pixeln == 0:  # Pprint sensor data every time the lights go around
+    # SENSORS - Print sensor data every time the lights go around
+    if current_pixel == 0:
+        x, y, z = cpx.acceleration
         print("Temperature: %0.1f *C" % cpx.temperature)
         print("Light Level: %d" % cpx.light)
-        x, y, z = cpx.acceleration
         print("Accelerometer: (%0.1f, %0.1f, %0.1f) m/s^2" % (x, y, z))
         print("-" * 40)
 
-    # SHAKE
-    if cpx.shake(shake_threshold=20):  # Look for a shake
-        cpx.pixels.fill(OFF)  # Turn off lights and pause sensor reporting
-        lights_on = not lights_on  # Switch light bool
-        led_on = not led_on  # Switch LED bool
+    # SHAKE - Look for a shake with the given threshold
+    if cpx.shake(shake_threshold=20):
+        # Turn off lights and pause sensor reporting
+        cpx.pixels.fill(BLACK)
 
-    # LED
-    cpx.red_led = led_on  # Turns on and off the little LED next to USB on
+        # Switch the neopixels on and off
+        lights_on = not lights_on
 
-    # loop to the beginning!
+        # Switch the red LED on and off
+        led_on = not led_on
+
+    # LED - Turns on and off the little LED next to USB on
+    cpx.red_led = led_on
+
+    # Go back to the beginning of the while True loop!
